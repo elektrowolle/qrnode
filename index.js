21490205b0fdf4d454a52ed9103543d7e8e11399
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-02-03 18:37:14 +0100

    exports the qrcode lib aswell
     #Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ module.exports.detect = function(im, callback){
   if(isUrl(im)){
       // Download image from url and process
       request({url:im, encoding:null}, function(err,res,body){
-        image = new Jimp(body, res.headers['content-type'], function () {  
-          qrcode.process(image.bitmap, callback);  
+        image = new Jimp(body, res.headers['content-type'], function () {
+          qrcode.process(image.bitmap, callback);
         });
       });
   }else{
       // Process image from local file
-      image = new Jimp(im, function () {  
+      image = new Jimp(im, function () {
          qrcode.process(image.bitmap, callback);
       });
    }
 }
+
+module.exports.qrcode = qrcode; 
 
 function isUrl(s){
   var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
